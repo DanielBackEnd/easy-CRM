2da2d2e4ea62c5c1845810a5c5cddee8ae24fd6b
Daniel Ilnicki <danielgithubb@gmail.com>
2023-06-15 11:53:36 +0200

    create style.css, create views for home page (/customers), create HTML structure for customers view and style by css

--- routers/customers.js
+++ routers/customers.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const {db} = require("../utils/db");
 
 const customersRouter = express.Router();
 
 customersRouter
     .get('/', (req, res) => {
-        res.send('working...');
+        res.render('customers/get-all', {
+            customers: db.getAll(),
+        });
+    })
+    .get('/:id', (req, res) => {
+        res.send('getting one customer.');
+    })
+    .post('/', (req, res) => {
+        res.send('adding new customer.');
+    })
+    .put('/:id', (req, res) => {
+        res.send('customer has been edited.');
+    })
+    .delete('/:id', (req, res) => {
+        res.send('customer has been deleted');
     })
 ;
 module.exports = {
